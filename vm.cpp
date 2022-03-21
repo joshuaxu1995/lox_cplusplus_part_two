52bd3ff3c20776a1923d281e714bb32f8a932568
Joshua Xu <joshxu95@gmail.com>
2022-03-21 11:52:33 -0500

    chapter 16 changes (#3)

--- vm.cpp
+++ vm.cpp
@@ -1,4 +1,5 @@
 #include "vm.h"
+#include "compiler.h"
 #include "common.h"
 #include "debug.h"
 
@@ -125,9 +126,8 @@ static InterpretResult run() {
 }
 
 
-InterpretResult interpret(Chunk* chunk) {
-    vm.chunk = chunk;
-    vm.ip = vm.chunk->code;
-    return run();
+InterpretResult interpret(const char* source) {
+    compile(source);
+    return INTERPRET_OK;
 }
 
